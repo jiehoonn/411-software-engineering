55b63bd75f1e1e91f7139d7443b61bf99ec940ef
Jiehoon Lee <jiehoonn@bu.edu>
2024-10-06 19:49:30 -0400

    add animal_management functions

--- HW3 Design/wildlife_tracker/animal_management/animal.py
+++ HW3 Design/wildlife_tracker/animal_management/animal.py
@@ -1,5 +1,10 @@
 from typing import Any, Optional
 
 class Animal:
+    animal_id: int
+    species: str
+    age: Optional[int] = None
+    health_status: Optional[str] = None
 
-    pass
+    def update_animal_details(self, animal_id: int, **kwargs: Any) -> None:
+        pass
